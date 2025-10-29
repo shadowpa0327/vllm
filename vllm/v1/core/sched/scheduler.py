5ffebea03c8d3310d20e9a1f16d52648658fbbab
Chi-Chih Chang <Chi-Chih Chang>
2025-10-29 07:44:59 +0000

    [feat] avoid recompute

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -498,7 +498,9 @@ class Scheduler(SchedulerInterface):
 
         # Next, schedule the WAITING requests.
         if not preempted_reqs:
-            while self.waiting and token_budget > 0:
+            #while self.waiting and token_budget > 0:
+            while self.waiting and token_budget > 0 and self.kv_cache_manager.usage < 0.9:
+            # (self.kv_cache_manager.block_pool.num_gpu_blocks) * 0.1
                 if len(self.running) == self.max_num_running_reqs:
                     break
 
